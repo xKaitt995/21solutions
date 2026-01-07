3cd441b4b43b0b28982eddf7ed8545d582f8b55a
xkaitt995 <leandro_aldea_95@hotmail.com>
2026-01-07 23:31:30 +0100

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
 
             <div className="hidden md:flex items-center space-x-8">
                 {/* Updated Links to match new sections */}
-                {["Como pensamos", "Que hacemos", "Quien somos", "Contactenos"].map((item) => (
+                {["Como pensamos", "Que hacemos", "Quien somos"].map((item) => (
                     <Link
                         key={item}
                         href={`#${item.toLowerCase().replace(" ", "-")}`}
